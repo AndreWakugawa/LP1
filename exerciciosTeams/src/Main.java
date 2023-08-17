3fab62c3e93303adf9416875343f7745f7c28e37
André Wakugawa <117671087+AndreWakugawa@users.noreply.github.com>
2023-08-16 22:38:45 -0300

    add ex (ex10, ex14, ex15, ex17, ex19)
    Ex10: Custo Final Carro
    Ex14: Maior que 10
    Ex15: Negativo ou Positivo
    Ex17: Aprovado ou Reprovado
    Ex19: Maior entre dois valores

--- exerciciosTeams/src/Main.java
+++ exerciciosTeams/src/Main.java
@@ -14,6 +14,16 @@ public class Main {
         ex7();
         System.out.println("Ex9: Reajuste de Salario");
         ex9();
+        System.out.println("Ex10: Custo Final Carro");
+        ex10();
+        System.out.println("Ex14: Maior que 10");
+        ex14();
+        System.out.println("Ex15: Negativo ou Positivo");
+        ex15();
+        System.out.println("Ex17: Aprovado ou Reprovado");
+        ex17();
+        System.out.println("Ex19: Maior entre dois valores");
+        ex19();
 
 
     }
@@ -91,5 +101,99 @@ public class Main {
         double reajuste = scanner.nextDouble();
         System.out.println("O seu salario reajustado é de: " +((salario) *(1 + reajuste /100)));
 
+    }
+
+
+    public static void ex10()   {
+        /* Ler custo de fabrica de um carro, calcular impostos e escrever custo final
+         * Distribuidor (%): 28
+         * Imposto (%): 45
+         */
+        Scanner scanner = new Scanner(System.in);
+
+        double distribuidor = 28;
+        double imposto = 45;
+        System.out.println("Insira custo de fabrica:");
+        double custoFabrica = scanner.nextDouble();
+
+        System.out.println("O valor final do carro é: " +(
+                    (custoFabrica)
+                    +(custoFabrica *(distribuidor /100))
+                    +(custoFabrica *(imposto /100))
+                )
+        );
+    }
+
+
+    public static void ex14()   {
+        /* Ler um valor e escrever a mensagem É MAIOR QUE 10! se o valor lido for maior que 10, caso
+         * contrário escrever NÃO É MAIOR QUE 10!
+         */
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Insira um numero:");
+        double numero = scanner.nextDouble();
+        if (numero >10) {
+            System.out.println("É MAIOR QUE 10!");
+        }
+
+        else {
+            System.out.println("NÃO É MAIOR QUE 10!");
+        }
+
+    }
+
+
+    public static void ex15()   {
+        // Ler um valor e escrever se é positivo ou negativo (considere o valor zero como positivo)
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Insira um numero:");
+        double numero = scanner.nextDouble();
+        if (numero >= 0)    {
+            System.out.println("O numero é positivo");
+        }
+        else {
+            System.out.println("O numero é negativo");
+        }
+
+    }
+
+
+    public static void ex17()   {
+        /* Ler notas de 2 avaliacoes de um aluno
+         * Calcular media aritmetica simples
+         * Escrever uma mensagem se aprovado ou nao (>=6 aprovado)
+         */
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Insira a nota da avaliacao 1:");
+        double aval1 = scanner.nextDouble();
+        System.out.println("Insira a nota da avaliacao 2:");
+        double aval2 = scanner.nextDouble();
+        double media = (aval1 +aval2) /2;
+        if (media <6)  {
+            System.out.println("Reprovado");
+        }
+        else {
+            System.out.println("Aprovado");
+        }
+    }
+
+
+    public static void ex19()   {
+        // Ler dois valores e escreva o maior deles
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Insira um valor:");
+        double valor1 = scanner.nextDouble();
+        System.out.println("Insira outro valor:");
+        double valor2 = scanner.nextDouble();
+        if (valor1 > valor2)    {
+            System.out.println(valor1);
+        }
+        else {
+            System.out.println(valor2);
+        }
     }
 }
